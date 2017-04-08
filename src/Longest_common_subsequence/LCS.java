06a6f5d94a05ac0e957329d0c187f0d6de9de77f
fragrantMiasmas <exreed@willamette.edu>
2017-04-08 00:12:03 -0700

    LCS-length function added

--- src/Longest_common_subsequence/LCS.java
+++ src/Longest_common_subsequence/LCS.java
@@ -11,4 +11,42 @@ package Longest_common_subsequence;
  */
 public class LCS {
     
+    public void LCS_length(char[] x, char[] y){
+        int m =  x.length;
+        int n = y.length;
+        
+        int[][] b = new int [n-1][m-1]; 
+        int[][] c = new int[n][m];
+        
+        for(int i=0; i<m; i++){ //set rows to zero
+            c[i][0] = 0;
+        }
+        
+        //may have to swithch 0 and j/i because java is different major than matlab
+        for(int j= 0; j<m; j++){ //set columns to zero
+            c[j][0] = 0;
+        }
+        
+        for (int i = 0;i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(x[i]==y[j]){
+                    c[i][j] = c[i-1][j-1]+1;
+                    b[i][j] = 1; //where one = diagonal arrow
+                }
+                else if(c[i-1][j]>=c[i][j-1]){
+                    c[i][j] = c[i-1][j];
+                    b[i][j] = 2; //where 2 = up arrow
+                }
+                else{
+                    c[i][j] = c[i][j-1];
+                    b[i][j] = 3; //where 3 = side arrow
+                }
+            }
+        }
+        
+        //later return 2-d arrays b and c
+    }
+    public void printLCS(int[][] b, char[] x){
+        
+    }
 }
